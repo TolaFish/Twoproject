d3d0db29562a62816143dc28833e0ec305815c9a
Pavel <Zamerchukpavel95@bk.ru>
2024-11-12 10:03:17 +0500

    Добавлены Endpoints

--- Twoproject/Program.cs
+++ Twoproject/Program.cs
@@ -1,6 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<User> repo = [];
 
+app.MapGet("/users", () => GetUsers());
+app.MapGet("/users/{id}", (int id) => FindUsers(id));
+app.MapPost("/users", (User user) => CreateUser(user));
+app.MapPut("/users", (User user) => UpdateUser(user));
+app.MapDelete("/users/{id}", (int id) => DeleteUser(id));
 app.Run();
+
+
+
+
+List<User> GetUsers()
+{
+    return repo;
+}
+void CreateUser(User user) => repo.Add(user);
+User void FindUser(int id) => repo.Find(x => x.Id == id);
+void UpdateUser(User dto)
+{
+    foreach (var u in repo)
+        if (u.Id == dto.Id)
+        {
+            if (u.Login != dto.Login && dto.Login != "" )
+            u.Login = dto.Login;
+            if (u.Password != dto.Password && dto.Password != "" )
+            u.Password = dto.Password;
+        }
+}
+void DeleteUser(int id) => repo.Remove(id);
+class User
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+}
